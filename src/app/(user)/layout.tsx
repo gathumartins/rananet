58201ded0364d8bbb6299a5d79df9b54721c9b14
gathumartins <gathumartins@gmail.com>
2024-06-05 14:05:14 +0300

    removed the commented code in the inner layout file

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -9,21 +9,10 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const res = await fetch("https://ranafrica.org/api/footer");
-  // const resLay = await fetch("https://ranafrica.org/api/layouts");
-  // const navs = await res.json();
-  // const lay = await resLay.json();
-  // const footNav = navs?.data?.menuItems?.nodes;
-  // const topNav = navs?.data?.topNav?.nodes;
-  // const menuFoot = flatListToHierarchical(footNav);
-  // const menuHeader = flatListToHierarchical(topNav);
-  // const sign = lay?.signup;
-  // console.log(sign);
+
   return (
     <Fragment>
-      {/* <Header navs={menuHeader} sign={sign} /> */}
       {children}
-      {/* <Footer navs={menuFoot} /> */}
     </Fragment>
   );
 }
