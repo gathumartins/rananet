c06f9f963a2c2fde90d4e687076ad7c70d004029
gathumartins <gathumartins@gmail.com>
2024-06-05 13:17:22 +0300

    uncommented contact us page

--- src/app/(user)/(getinvolved)/contactus/page.tsx
+++ src/app/(user)/(getinvolved)/contactus/page.tsx
@@ -27,46 +27,45 @@ import ContactForm from "@/components/ContactForm";
 //   };
 // }
 async function page() {
-  // const myconts = await fetch(`${process.env.BASE_URL}/api/contacts`, {
-  //   cache: "no-store",
-  // });
-  // const data = await myconts.json();
+  const myconts = await fetch(`${process.env.BASE_URL}/api/contacts`, {
+    cache: "no-store",
+  });
+  const data = await myconts.json();
 
-  // const markup = { __html: data.content };
-  // const bgImage = data.featuredImage.node.sourceUrl;
+  const markup = { __html: data.content };
+  const bgImage = data.featuredImage.node.sourceUrl;
 
   return (
-    <h1>Test</h1>
-    // <main>
-    //   <header
-    //     className="topBann"
-    //     style={{ backgroundImage: `url(${bgImage})` }}
-    //   >
-    //     <div className="container flex justify-start place-items-center">
-    //       <h1 className="topHeroCont capitalize">{data.title}</h1>
-    //     </div>
-    //   </header>
-    //   <section className="py-[40px]">
-    //     <div className="container">
-    //       <Row>
-    //         <Col
-    //           xs={12}
-    //           sm={12}
-    //           md={12}
-    //           lg={12}
-    //           className="universal mb-[30px]"
-    //         >
-    //           <h2 className="mt-0 capitalize">{data.title}</h2>
-    //           <div
-    //             className="universal [&>p]:mb-[20px]"
-    //             dangerouslySetInnerHTML={markup}
-    //           ></div>
-    //         </Col>
-    //         <ContactForm />
-    //       </Row>
-    //     </div>
-    //   </section>
-    // </main>
+    <main>
+      <header
+        className="topBann"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
+        <div className="container flex justify-start place-items-center">
+          <h1 className="topHeroCont capitalize">{data.title}</h1>
+        </div>
+      </header>
+      <section className="py-[40px]">
+        <div className="container">
+          <Row>
+            <Col
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              className="universal mb-[30px]"
+            >
+              <h2 className="mt-0 capitalize">{data.title}</h2>
+              <div
+                className="universal [&>p]:mb-[20px]"
+                dangerouslySetInnerHTML={markup}
+              ></div>
+            </Col>
+            <ContactForm />
+          </Row>
+        </div>
+      </section>
+    </main>
   );
 }
 
