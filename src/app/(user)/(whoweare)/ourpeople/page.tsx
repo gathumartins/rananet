537a50d2ffd58d95eee9151f81be870d17241730
gathumartins <gathumartins@gmail.com>
2024-06-05 14:29:32 +0300

    uncommented theseo metadata inour people page

--- src/app/(user)/(whoweare)/ourpeople/page.tsx
+++ src/app/(user)/(whoweare)/ourpeople/page.tsx
@@ -2,28 +2,28 @@ import { Metadata } from "next";
 import React from "react";
 import Staff from "../../../../components/Staff";
 
-// export async function generateMetadata(): Promise<Metadata> {
-//   // fetch data
-//   const seo = await fetch(
-//     `${process.env.BASE_URL}/api/pagesSeo?id=cG9zdDo2MA==`,
-//     {
-//       next: { revalidate: 60 },
-//     }
-//   ).then((res) => res.json());
+export async function generateMetadata(): Promise<Metadata> {
+  // fetch data
+  const seo = await fetch(
+    `${process.env.BASE_URL}/api/pagesSeo?id=cG9zdDo2MA==`,
+    {
+      next: { revalidate: 60 },
+    }
+  ).then((res) => res.json());
 
-//   // optionally access and extend (rather than replace) parent metadat
+  // optionally access and extend (rather than replace) parent metadat
 
-//   return {
-//     title: seo.pageSeo.title,
-//     description: seo.pageSeo.metaDesc,
-//     alternates: {
-//       canonical: seo.pageSeo.opengraphUrl,
-//     },
-//     openGraph: {
-//       images: [seo.pageSeo.opengraphImage.sourceUrl],
-//     },
-//   };
-// }
+  return {
+    title: seo.pageSeo.title,
+    description: seo.pageSeo.metaDesc,
+    alternates: {
+      canonical: seo.pageSeo.opengraphUrl,
+    },
+    openGraph: {
+      images: [seo.pageSeo.opengraphImage.sourceUrl],
+    },
+  };
+}
 async function page() {
   const myconts = await fetch(`${process.env.BASE_URL}/api/people`, {
     cache: "no-store",
